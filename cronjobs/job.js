8e381f6b9a5d25ee7b55cd9fdf50034e3e70a8c1
coppercodes <coppercodes@gmail.com>
2022-06-03 18:13:52 +0530

    Cron Jobs

--- cronjobs/job.js
+++ cronjobs/job.js
@@ -2,25 +2,71 @@
 var CronJob = require('cron').CronJob;
 var fs=require('fs');
 var unlink=require('fs').unlink
+var icmp = require('icmp')
+var nodemailer = require("nodemailer");
 
-var job = new CronJob('0 0 * * *', function() {
+async function main() {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'akshay.shirwaikar@gmail.com', // generated ethereal user
+      pass: 'vxxtgamwhozzddqg', // generated ethereal password
+    },
+  });
+
+    let info = await transporter.sendMail({
+    from: 'akshay.shirwaikar@gmail.com', // sender address
+    to: "coppercodes@gmail.com", // list of receivers
+    subject: "Hello", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+
+var job1 = new CronJob('0 0 */28 * *', function() {
+
+  // ping to a host
+  // icmp.send('139.59.80.152', "Hey, I'm sending a message!")
+  //   .then(obj => {
+  //       console.log(obj.open ? 'Done' : 'Failed')
+  //   })
+  //   .catch(err => console.log(err));
+
+
 
   const testFolder = '../coreservice/Logs/';
-  const testFile = testFolder+fs.readdirSync(testFolder)[0];
+  const testFiles = testFolder+fs.readdirSync(testFolder);
 
-  
+  const logFiles = testFiles.split(',')
 
-  console.log('currentFile',testFile);
-
-  directoryLength=fs.readdirSync(testFolder).length
-
-
-directoryLength>7?removeAFile(testFile):null;
-
+  for(var i=0; i<logFiles.length;i++){
+    directoryLength=fs.readdirSync(testFolder).length
+    console.log('currentFile',logFiles[i]);
+    if(i==0){
+      directoryLength>0?removeAFile(logFiles[i]):null
+    }else{
+      directoryLength>7?removeAFile(testFolder+logFiles[i]):null 
+    }
+  }
   
 }, null, true, 'Asia/Kolkata');
 
-job.start();
+var job2 = new CronJob('* * * * *',()=>{
+  icmp.ping('134.209.154.152',500)
+  .then(obj => {console.log(obj.open ? 'Chalta' : 'Failed')})
+  .catch(err=>console.log(err))
+
+})
+
+job1.start();
+job2.start();
 
 
 var removeAFile=(path)=>{
